29b2cc043111bc2cf9fa9b64f22677c2e4c4f20d
sifter0926 <sifter0926@gmail.com>
2025-03-12 10:24:02 +0900

    111

---
+++ java202503/src/java202503/Ex01.java
@@ -0,0 +1,9 @@
+package java202503;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("123");
+	}
+
+}
---
+++ java202503/src/java202503/Ex04.java
@@ -0,0 +1,10 @@
+package java202503;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
