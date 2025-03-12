2ca589981a52217f054c631d11bd37ff3b683ea6
sifter0926 <sifter0926@gmail.com>
2025-03-12 16:36:57 +0900

    9번까지

---
+++ java202503/src/java202503/Ex09.java
@@ -0,0 +1,39 @@
+package java202503;
+
+import java.util.Scanner;
+
+public class Ex09 
+{
+	public static void main(String[] args) {
+		Scanner sc1 = new Scanner(System.in);
+		System.out.print("원의 중심(x, y)과 반지름 (r) 입력>>");
+		int x = sc1.nextInt();
+		int y = sc1.nextInt();
+		double r = sc1.nextDouble();
+		
+		System.out.print("한 점(x, y)입력>>");
+		int x1 = sc1.nextInt();
+		int y1 = sc1.nextInt();
+		
+		double l = Math.sqrt((x1-x)*(x1-x) + (y1-y)*(y1-y));
+		
+		if(l>r)
+			System.out.println("점("+x1+","+y1+")는 원 밖에");
+		else
+			System.out.println("점("+x1+","+y1+")는 원 안에");
+		
+
+		sc1.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
